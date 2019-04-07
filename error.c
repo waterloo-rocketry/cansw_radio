b31c0896f6f5f46578e1d1dd62cbcd854fbfb435
Alexander Kitaev <alex.kitaev58@gmail.com>
2019-04-07 17:04:01 -0400

    Changed error message ring buffer to use a statically allocated array

--- error.c
+++ error.c
@@ -1,6 +1,7 @@
 #include "error.h"
 
-static char **err_msg_ring_buf = NULL;
+static char err_msg_ring_buf[ERROR_MESSAGE_RING_BUFFER_SIZE]
+    [ERROR_COMMAND_LENGTH];
 static uint8_t err_msg_buf_size = 0;
 static uint8_t err_msg_buf_write = 0;
 static uint8_t err_msg_buf_read = 0;
@@ -10,10 +11,6 @@ void report_error(uint8_t board_id,
                   uint8_t byte4, uint8_t byte5,
                   uint8_t byte6, uint8_t byte7)
 {
-    if(err_msg_ring_buf == NULL) {
-        err_msg_ring_buf = malloc(
-            ERROR_MESSAGE_RING_BUFFER_SIZE * sizeof(error_t));
-    }
 
     error_t err;
     err.board_id = board_id;
@@ -23,10 +20,8 @@ void report_error(uint8_t board_id,
     err.byte6 = byte6;
     err.byte7 = byte7;
 
-    char *serialized_error = malloc(ERROR_COMMAND_LENGTH * sizeof(char));
+    char *serialized_error = err_msg_ring_buf[err_msg_buf_write];
     serialize_error(&err, serialized_error);
-
-    err_msg_ring_buf[err_msg_buf_write] = serialized_error;
     ++err_msg_buf_size;
     ++err_msg_buf_write;
 }
@@ -37,11 +32,10 @@ bool get_next_serialized_error(char *output)
     if(err_msg_buf_size == 0) return false;
 
     char *serialized_error = err_msg_ring_buf[err_msg_buf_read];
+    memcpy(output, serialized_error, ERROR_COMMAND_LENGTH);
+
     ++err_msg_buf_read;
     --err_msg_buf_size;
-
-    memcpy(output, serialized_error, ERROR_COMMAND_LENGTH);
-    free(serialized_error);
 
     return true;
 }
