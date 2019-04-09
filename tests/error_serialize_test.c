d0c134d827aa92e9e776df82c3fd4cd404bb6d25
Aaron Morrison <akmorris@edu.uwaterloo.ca>
2019-04-08 22:20:58 -0400

    Add randomized test to error serializer
    Make sure report_error can handle 1000 errors. Not all at the same time,
    but eventually.

--- tests/error_serialize_test.c
+++ tests/error_serialize_test.c
@@ -2,6 +2,7 @@
 #include "error.h"
 #include "serialize.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #define COLOR_GREEN "\x1B[32m"
 #define COLOR_RED   "\x1B[31m"
@@ -48,6 +49,51 @@ int main()
                deserialized_err.err_type == err_type &&
                deserialized_err.byte4 == mV_high &&
                deserialized_err.byte5 == mV_low, "error values aren't what we expected");
+
+    //let's try to randomize 1000 error messages, serialize report them in batches of
+    //1-10, then pull them out and compare them
+    uint32_t randomized_error_count = 0;
+    bool randomized_test_passed = true;
+    while(randomized_error_count < 1000) {
+        uint8_t num_errors = rand() % 10;
+        //generate and report that many errors. Then serialize those
+        //errors so we know what _should_ come out of get_next_error
+        char serialized_errors[num_errors][ERROR_COMMAND_LENGTH];
+        for (int i = 0; i < num_errors; ++i) {
+            //randomize an error
+            uint8_t board_id = rand() % 16;
+            enum BOARD_STATUS error_type = rand() % 20;
+            uint8_t byte4 = rand() % 256;
+            uint8_t byte5 = rand() % 256;
+            uint8_t byte6 = rand() % 256;
+            uint8_t byte7 = rand() % 256;
+            report_error(board_id, error_type, byte4, byte5, byte6, byte7);
+            error_t error = {
+                .board_id = board_id,
+                .err_type = error_type,
+                .byte4 = byte4,
+                .byte5 = byte5,
+                .byte6 = byte6,
+                .byte7 = byte7,
+            };
+            serialize_error(&error, serialized_errors[i]);
+        }
+
+        //call get_next_serialized_error that many times, make sure they
+        //all line up
+        for (int i = 0; i < num_errors; ++i) {
+            char next_error[ERROR_COMMAND_LENGTH];
+            get_next_serialized_error(next_error);
+            if(strcmp(next_error, serialized_errors[i]) ){
+                UNIT_TEST(false, "randomized test");
+                randomized_test_passed = false;
+            }
+        }
+        randomized_error_count += num_errors;
+    }
+    if (randomized_test_passed) {
+        UNIT_TEST(true, "randomized test");
+    }
 
 
     printf("%s Test Results: %i tests, %i passed, %i %sfailed%s\n",
