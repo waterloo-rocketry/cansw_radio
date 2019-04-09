1cf2ee28c944e026f3a076781b5f826ce918a678
Alexander Kitaev <alex.kitaev58@gmail.com>
2019-04-08 22:33:32 -0400

    Fixed ring buffer positions not wrapping around the buffer

--- error.c
+++ error.c
@@ -1,5 +1,6 @@
 #include "error.h"
 
+
 static char err_msg_ring_buf[ERROR_MESSAGE_RING_BUFFER_SIZE]
     [ERROR_COMMAND_LENGTH];
 static uint8_t err_msg_buf_size = 0;
@@ -11,7 +12,8 @@ void report_error(uint8_t board_id,
                   uint8_t byte4, uint8_t byte5,
                   uint8_t byte6, uint8_t byte7)
 {
-
+    // Check if the buffer is full
+    if(err_msg_buf_size == ERROR_MESSAGE_RING_BUFFER_SIZE) return;
     error_t err;
     err.board_id = board_id;
     err.err_type = error_type;
@@ -24,6 +26,7 @@ void report_error(uint8_t board_id,
     serialize_error(&err, serialized_error);
     ++err_msg_buf_size;
     ++err_msg_buf_write;
+    err_msg_buf_write %= ERROR_MESSAGE_RING_BUFFER_SIZE;
 }
 
 bool get_next_serialized_error(char *output)
@@ -35,6 +38,7 @@ bool get_next_serialized_error(char *output)
     memcpy(output, serialized_error, ERROR_COMMAND_LENGTH);
 
     ++err_msg_buf_read;
+    err_msg_buf_read %= ERROR_MESSAGE_RING_BUFFER_SIZE;
     --err_msg_buf_size;
 
     return true;
