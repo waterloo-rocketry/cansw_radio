0d31c9c944c1acf13901a73e08f44a73d9a45817
Aaron Morrison <akmorris@edu.uwaterloo.ca>
2019-04-21 16:33:47 -0400

    Report error when ADC reports wrong value
    After starting every ADC conversion, check all the last reported values
    for being out of range. If they're out of range, report an error. Rate
    limit these errors to one error per second, to not overwhelm the bus.

--- analog.c
+++ analog.c
@@ -1,6 +1,8 @@
 #include "analog.h"
 #include "platform.h"
 #include "pic18_time.h"
+#include "message_types.h"
+#include "error.h"
 #include <xc.h>
 
 //global variables. Until we have a better way of handling analog
@@ -9,6 +11,9 @@
 static uint16_t analog_last_batt_voltage = 0;
 static uint16_t analog_last_batt_current = 0;
 static uint16_t analog_last_bus_current = 0;
+
+//private function declarations
+static void check_analog_values(void);
 
 uint16_t analog_get_vin_mv(void)
 {
@@ -154,5 +159,75 @@ void analog_heartbeat(void)
     if (millis() - last_read_ms > 100) {
         last_read_ms = millis();
         analog_read_all_values();
+        if (last_read_ms > 0) {
+            //we've already done one conversion, so check that the values for
+            //all the analog signals are within range
+            check_analog_values();
+        }
+    }
+}
+
+/*
+ * Check the input current, output current, and input voltage. If any of them
+ * are out of the expected range, report an error.
+ *
+ * Current expected values:
+ * Input voltage: 10V < Vin < 14V
+ * Input current: Iin < 2A
+ * Output current: Iout < 2A
+ *
+ * Don't report an error on each of these values more than once every second,
+ * just so we don't overwhelm the bus or the radio
+ */
+static void check_analog_values(void)
+{
+    uint16_t vin = analog_get_vin_mv();
+    uint16_t iin = analog_get_ibatt_ma();
+    uint16_t iout = analog_get_ibus_ma();
+
+    uint32_t ms_last_vin_error = 0;
+    uint32_t ms_last_iin_error = 0;
+    uint32_t ms_last_iout_error = 0;
+
+    if (vin < 10000) {
+        if (millis() - ms_last_vin_error > 1000) {
+            ms_last_vin_error = millis();
+            report_error(BOARD_UNIQUE_ID,
+                         E_BATT_UNDER_VOLTAGE,
+                         vin >> 8,
+                         vin & 0xff,
+                         0, 0);
+        }
+    } else if (vin > 14000) {
+        if (millis() - ms_last_vin_error > 1000) {
+            ms_last_vin_error = millis();
+            report_error(BOARD_UNIQUE_ID,
+                         E_BATT_OVER_VOLTAGE,
+                         vin >> 8,
+                         vin & 0xff,
+                         0, 0);
+        }
+    }
+
+    if (iin > 2000) {
+        if (millis() - ms_last_iin_error > 1000) {
+            ms_last_iin_error = millis();
+            report_error(BOARD_UNIQUE_ID,
+                         E_BATT_OVER_CURRENT,
+                         iin >> 8,
+                         iin & 0xff,
+                         0, 0);
+        }
+    }
+
+    if (iout > 2000) {
+        if (millis() - ms_last_iout_error > 1000) {
+            ms_last_iout_error = millis();
+            report_error(BOARD_UNIQUE_ID,
+                         E_BUS_OVER_CURRENT,
+                         iout >> 8,
+                         iout & 0xff,
+                         0, 0);
+        }
     }
 }
